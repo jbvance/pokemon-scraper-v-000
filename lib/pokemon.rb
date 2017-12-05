3c0eb48ae126bac8ed7dd9f936db22a5d879484f
Jason Vance <jbvance@gmail.com>
2017-12-05 00:26:29 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,4 @@
+require 'pry' 
 class Pokemon
   attr_accessor :id, :name, :type, :db
   def initialize(id:, name:, type:, hp: nil, db:)
@@ -6,5 +7,10 @@ class Pokemon
 
     def self.save(name, type, db)
        db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)",name, type)
+    end
+
+    def self.find(id, db)
+      test = db.execute("SELECT * FROM pokemon WHERE id=?", id)
+      binding.pry
     end
 end
